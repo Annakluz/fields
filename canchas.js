c05bf3eeaf990834365ba509a06f1a6f83909157
Annakluz <anna.luz.duran@gmail.com>
2017-04-11 22:51:20 -0500

    JS, HTML & CSS RUNNING!

--- canchas.js
+++ canchas.js
@@ -1,8 +1,41 @@
-//VARIABLES GLO
+//GLOBAL VARIABLES
 let soccer = document.getElementById("soccer");
 let basketball = document.getElementById("basketball");
 let tenis = document.getElementById("tenis");
+let soccerField = document.getElementById("soccerField");
+let basketField = document.getElementById("basketField");
+let tennisField = document.getElementById("tennisField");
 
-function soccer{
+//THIS FUNCTION HIDES ALL THE FIELDS WHEN LOADING STARTS
+window.onload= function hide(){
+    soccerField.style.display= "none";
+    basketField.style.display = "none";
+    tennisField.style.display = "none";
     
 }
+
+//SOCCER 
+soccer.addEventListener("click", function(){
+    soccerField.style.display = "block";
+});
+soccer.addEventListener("dblclick", function(){
+    soccerField.style.display = "none";
+});
+
+//BASKET
+basketball.addEventListener("click", function(){
+    basketField.style.display = "block";
+});
+basketball.addEventListener("dblclick", function(){
+    basketField.style.display = "none";
+});
+
+//TENNIS
+tenis.addEventListener("click", function(){
+    tennisField.style.display = "block";
+});
+tenis.addEventListener("dblclick", function(){
+    tennisField.style.display = "none";
+});
+
+
